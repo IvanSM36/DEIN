e91aaa047a2b23d778549ad3874301af86783909
IvanSM <ivan_sm@hotmail.es>
2023-02-06 13:36:15 +0100

    Initial commit

---
+++ SegundoTrimestre/WPF_Relacion13_IvanSM/WPF_ConexionDB_IvanSM/AdminDB.cs
@@ -0,0 +1,42 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_ConexionDB_IvanSM
+{
+    internal class AdminDB
+    {
+        static MySqlConnection Conex = new MySqlConnection();
+        static string serv = "server=212.227.147.133;";
+        static string db = "database=ZooTerra;";
+        static string usuario = "id=root;";
+        static string pwd = "password=root;";
+        string CadenaDeConexion = serv + db + usuario + pwd;
+        static MySqlCommand Comando = new MySqlCommand();
+        static MySqlDataAdapter Adaptador = new MySqlDataAdapter();
+
+        public void Conectar()
+        {
+            try
+            {
+                Conex.ConnectionString = CadenaDeConexion;
+                Conex.Open();
+                MessageBox.Show("La BD esta ahora conectada");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrio un error al conectar a la BD");
+                throw;
+            }
+        }
+
+        public static void Desconectar()
+        {
+            Conex.Close();
+        }
+    }
+}
---
+++ SegundoTrimestre/WPF_Relacion13_IvanSM/WPF_ConexionDB_IvanSM/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ConexionDB_IvanSM
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AdminDB db = new AdminDB();
+            db.Conectar();
+        }
+        private void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=212.227.147.133;id=root;password=root;database=ZooTerra;";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Successful connection to the database.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error connecting to the database: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
