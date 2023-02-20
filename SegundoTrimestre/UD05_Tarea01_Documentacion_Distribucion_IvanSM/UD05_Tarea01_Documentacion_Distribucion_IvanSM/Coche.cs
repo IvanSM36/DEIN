6f1061ca481d7f00acc988fec70b880e43a0bc19
IvanSM <ivan_sm@hotmail.es>
2023-02-20 13:06:53 +0100

    Initial commit

---
+++ SegundoTrimestre/UD05_Tarea01_Documentacion_Distribucion_IvanSM/UD05_Tarea01_Documentacion_Distribucion_IvanSM/Coche.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UD05_Tarea01_Documentacion_Distribucion_IvanSM
+{
+    internal class Coche
+    {
+        // Atributos
+        private int id;
+        private String matricula;
+        private String marca;
+        private String modelo;
+        private String color;
+
+        // Constructores
+
+        /// <summary>
+        /// Constructor del objeto coche vacio
+        /// </summary>
+        public Coche()
+        {
+        }
+
+        /// <summary>
+        /// Constructor del objeto coche con todos los parametros
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="matricula"></param>
+        /// <param name="marca"></param>
+        /// <param name="modelo"></param>
+        /// <param name="color"></param>
+        public Coche(int id, string matricula, string marca, string modelo, string color)
+        {
+            this.id = id;
+            this.matricula = matricula;
+            this.marca = marca;
+            this.modelo = modelo;
+            this.color = color;
+        }
+
+        // Getters and setters
+
+        /// <value>Get y Set del atributo ID </value>    
+        public int Id { get => id; set => id = value; }
+
+        /// <value>Get y Set del atributo matricula </value>    
+        public string Matricula { get => matricula; set => matricula = value; }
+
+        /// <value>Get y Set del atributo marca </value>    
+        public string Marca { get => marca; set => marca = value; }
+
+        /// <value>Get y Set del atributo modelo </value>    
+        public string Modelo { get => modelo; set => modelo = value; }
+
+        /// <value>Get y Set del atributo color </value>    
+        public string Color { get => color; set => color = value; }
+
+        // Metodos
+
+        /// <summary>
+        /// Metodo que muestra un string simulando que acelera
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns>String</returns>
+        public String acelerar()
+        {
+            return "Fiiiiumm"; 
+        }
+    }
+}
--- SegundoTrimestre/UD05_Tarea01_Documentacion_Distribucion_IvanSM/UD05_Tarea01_Documentacion_Distribucion_IvanSM/MainWindow.xaml.cs
+++ SegundoTrimestre/UD05_Tarea01_Documentacion_Distribucion_IvanSM/UD05_Tarea01_Documentacion_Distribucion_IvanSM/MainWindow.xaml.cs
@@ -30,13 +30,19 @@ namespace UD05_Tarea01_Documentacion_Distribucion_IvanSM
             imgEstado.Source = new BitmapImage(new Uri("/image/off.png", UriKind.Relative));
         }
 
+        // Comentar una linea
 
-
-
-        /*
-         * Metodo que comprueba la conexion.
-         * 
+        /* Comentar varias lineas
+         * <summary>: tag que muestra una pequeña definicion. 
+         * <param>: tag que muestra el tipo de dato que recoge por parametro
          */
+
+
+        /// <summary>
+        /// Metodo que comprueba la conexion.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnPruebaConectar(object sender, RoutedEventArgs e)
         {
             // Obtiene la cadena de conexión de la BBDD.        
@@ -65,57 +71,67 @@ namespace UD05_Tarea01_Documentacion_Distribucion_IvanSM
             }
         }
 
-        /*
-         * Metodo que abre la ventana MostrarDatos
-         * 
-         */
+        /// <summary>
+        ///  Metodo que abre la ventana MostrarDatos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnMostrarDatos(object sender, RoutedEventArgs e)
         {
             MostrarDatos md = new MostrarDatos();
             md.Show();
         }
 
-        /*
-         * Metodo que abre la ventana de GenerarInforme
-         * 
-         */
+        /// <summary>
+        /// Metodo que abre la ventana de GenerarInforme
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnGenerarInforme(object sender, RoutedEventArgs e)
         {
             GenerarInforme gi = new GenerarInforme();
             gi.Show();
         }
 
-        /*
-         * Metodo que abre la ventana InsertarDatos
-         */
+        
+        /// <summary>
+        /// Metodo que abre la ventana InsertarDatos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnInsertarDatos(object sender, RoutedEventArgs e)
         {
             InsertarDatos id = new InsertarDatos();
             id.Show();
         }
 
-        /*
-         * Metodo que abre la ventana ModificarDatos
-         *
-         */
+        /// <summary>
+        /// Metodo que abre la ventana ModificarDatos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnModificarDatos(object sender, RoutedEventArgs e)
         {
             ModificarDatos md = new ModificarDatos();
             md.Show();
         }
 
-        /* *
-         * Metodo que abre la ventana BorrarDatos
-         */
+        /// <summary>
+        /// Metodo que abre la ventana BorrarDatos
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnBorrarDatos(object sender, RoutedEventArgs e)
         {
             BorrarDatos bd = new BorrarDatos();
             bd.Show();
         }
 
-        /*
-         *  Metodo que cierra la ventana
-         */
+        /// <summary>
+        /// Metodo que cierra la ventana
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCerrarVentana(object sender, RoutedEventArgs e)
         {
             this.Close();
